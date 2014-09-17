4b9c9237d6f650d6d3e7812d86f003432da09b86
Stuart Terrett <shterrett@gmail.com>
2014-09-16 22:21:46 -0400

    Models gravity

---
+++ src/nature_of_code/ch_2_forces/gravity.clj
@@ -0,0 +1,32 @@
+(ns nature_of_code.ch_2_forces.gravity
+  (:require [quil.core :as q]))
+
+(def sketch-size 500)
+
+(defn bounce? [pos vel]
+  (let [new-pos (+ pos vel)]
+    (or (> new-pos sketch-size)
+        (< new-pos 0))))
+
+(defn accelerate [pos vel accel]
+  (if (bounce? pos vel)
+    (* -1 vel)
+    (+ vel accel)))
+
+(defn update [{gravity :gravity {:keys [x y dx dy]} :ball}]
+  (let [new-y (+ y dy)
+        new-x (+ x dx)]
+    {:gravity gravity
+     :ball {:x new-x :y new-y
+            :dx (accelerate new-x dx 0) :dy (accelerate new-y dy gravity)}}))
+
+(defn setup []
+  (q/frame-rate 30)
+  (q/color-mode :rgb)
+  {:gravity 1
+   :ball {:x 0 :y 500 :dx 15 :dy -15}})
+
+(defn draw [{:keys [gravity ball]}]
+  (q/background 153 236 234)
+  (q/fill 255 111 23)
+  (q/ellipse (:x ball) (:y ball) 10 10))
---
+++ src/nature_of_code/ch_2_forces/gravity_sketch.clj
@@ -0,0 +1,15 @@
+(ns nature_of_code.ch_2_forces.gravity_sketch
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [nature_of_code.ch_2_forces.gravity :as g]))
+
+(q/defsketch vector_intro
+  :title "Vector Intro"
+  :size [g/sketch-size g/sketch-size]
+  :setup g/setup
+  :update g/update
+  :draw g/draw
+  :middleware [m/fun-mode])
+
+(defn reload []
+  (use :reload 'nature_of_code.ch_2_forces.gravity))
