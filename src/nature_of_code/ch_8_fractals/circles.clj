2b7374bc5eabe14594bf5dac750db9c8bdf87c5e
Stuart Terrett <shterrett@gmail.com>
2014-12-08 21:14:02 -0500

    Circles fractals
    Code assistance provided by
    http://www.reddit.com/r/Clojure/comments/2on0sj/multibranch_recursion_without_blowing_the_stack/

--- src/nature_of_code/ch_8_fractals/circles.clj
+++ src/nature_of_code/ch_8_fractals/circles.clj
@@ -5,23 +5,41 @@
 (def sketch-size 400)
 
 (defn setup []
-  20)
+  (q/background 39 117 34)
+  100)
 
 (defn update [radius]
   radius)
 
-(defn draw-circle [radius x y]
-  (let [r2 (/ radius 2)]
-    (if (> r2 1)
-      (do
-        (q/ellipse x y radius radius)
-        (draw-circle r2 (+ x r2) y)
-        (draw-circle r2 x (+ y r2))
-        (draw-circle (- x r2) y r2)
-        (draw-circle x (- y r2) x)))))
 
+;;(defn draw-circle [radius x y]
+;;  (let [r2 (/ radius 2)]
+;;    (if (> r2 4)
+;;      (do
+;;        (q/ellipse x y radius radius)
+;;        (draw-circle r2 (+ x r2) y)
+;;        (draw-circle r2 x (+ y r2))
+;;        (draw-circle r2 (- x r2) y)
+;;        (draw-circle r2 x (- y r2))))))
+
+(defn circles
+  ([r x y] (circles [[r x y]]))
+  ([[[r x y :as h] & t]]
+   (if (nil? h) []
+     (cons h (lazy-seq (circles
+                         (let [r1 (/ r 2)]
+                           (if (> r1 2)
+                             (conj t
+                                   [r1 (+ x r1) y]
+                                   [r1 (- x r1) y]
+                                   [r1 x (+ y r1)]
+                                   [r1 x (- y r1)])
+                             t))))))))
+
+(defn draw-circles [r x y]
+  (doseq [[r x y] (circles r x y)]
+    (q/ellipse x y r r)))
 
 (defn draw [radius]
-  (q/background 39 117 34)
   (q/stroke 154 0 255)
-  (draw-circle radius (/ sketch-size 2) (/ sketch-size)))
+  (draw-circles radius (/ sketch-size 2) (/ sketch-size 2)))
